c546e0c48207ed33b1abc675e4e28a396d3a4d32
krsamir <samirkumar2527@gmail.com>
2020-09-10 11:05:00 +0530

    Reading Notes

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -54,8 +54,15 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Reading a note',
-    handler(){
-        console.log('Reading a note')
+    builder:{
+        title:{
+            describe: 'Note Title',
+            demandOption:true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.readNotes(argv.title)
     }
 })
 
--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = ()=>{
-    return "Your notes ...";
-}
-
 const addNote = (title, body)=>{
     const notes = loadNotes();
     const duplicateNotes = notes.filter((note)=>note.title === title)
-    if(duplicateNotes.length === 0){
+    const duplicateNote = notes.find((note)=>note.title === title)
+    if(!duplicateNote){
         notes.push({
             title:title,
             body:body
@@ -35,7 +32,8 @@ const loadNotes = ()=>{
     }
     
 }
-/*********************************************************************************** */
+
+
 const removeNotes = function(title){
     const notes = loadNotes();
     const notesToKeep = notes.filter((note)=> note.title !== title)
@@ -56,9 +54,21 @@ const listNotes = () =>{
         console.log(note.title)
     })
 }
+
+const readNotes = (title) =>{
+    const notes = loadNotes()
+    const note = notes.find((note)=>note.title === title)
+    
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.red.inverse('Note not Found!'))
+    }
+}
 module.exports ={
-    getNotes: getNotes,
     addNote: addNote,
     removeNotes: removeNotes,
     listNotes: listNotes,
+    readNotes: readNotes,
 }
